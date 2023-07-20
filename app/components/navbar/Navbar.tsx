e0617ec8328a43e61ed04abc9e7644ef0e465c89
romanpoudel <romanpoudel325@gmail.com>
2023-07-20 21:28:29 +0545

    material ui badge

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import { FiShoppingCart } from "react-icons/fi"
 import { HiOutlineUser } from "react-icons/hi"
 import { FiSearch } from "react-icons/fi"
-import { Roboto,Rubik } from 'next/font/google'
+import { Roboto, Rubik } from 'next/font/google'
+import { Badge } from "@mui/material";
 
 
 
 
 
 const Navbar = () => {
-    return <div className="h-16 sticky top-0 flex items-center justify-between mx-8">
-        <div>
-            <h2 className="font-logo text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ">OnlineStore</h2>
+    return <div className="h-16 sticky top-0 flex items-center justify-between mx-2 sm:mx-8">
+        <div className="hidden sm:block">
+            <h2 className=" font-logo text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ">OnlineStore</h2>
         </div>
 
         <div className="relative flex items-center text-gray-400 focus-within:text-gray-600">
@@ -19,8 +20,10 @@ const Navbar = () => {
             <FiSearch size={20} color="gray" className="absolute  pointer-events-none font-semibold ml-2" />
         </div>
         <div className="flex  justify-between">
-            <FiShoppingCart size={20} className="m-4"/>
-            <HiOutlineUser size={20} className="m-4" />
+            <Badge badgeContent={4} color="primary">
+                <FiShoppingCart size={22} />
+            </Badge>
+            <HiOutlineUser size={22} className="ml-10" />
         </div>
     </div>;
 };
