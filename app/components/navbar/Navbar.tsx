6952cf6b5acfd5a96dff27b12a9b4b258d80198d
romanpoudel <romanpoudel325@gmail.com>
2023-07-24 21:05:05 +0545

    offer responsive

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     const data = useSelector((state: any) => state.cart.itemsNumber);
     // const cart = useSelector((state: any) => state.cart.cartProducts);
     console.log("🚀 ~ file: Navbar.tsx:12 ~ Navbar ~ data:", data)
-    return <div className="h-16 sticky top-0 flex items-center justify-between px-2 sm:px-8 z-20 bg-gray-50 bg-opacity-95">
+    return <div className="max-w-[1400px] mx-auto h-16 sticky top-0 flex items-center justify-between px-4 sm:px-8 md:px-4 z-20 bg-gray-50 bg-opacity-95">
         <div className="hidden sm:block">
             <Link href="/"> <h2 className=" font-logo text-2xl font-extrabold text-transparent bg-clip-text  bg-gradient-to-r from-green-400 via-yellow-400 to-pink-400">OnlineStore</h2> </Link>
         </div>
@@ -21,7 +21,7 @@ const Navbar = () => {
         <div className="flex  justify-between">
             <Badge badgeContent={data} color="success">
                 <Link href="/cart">
-                    <FiShoppingCart size={22} color="green"/>
+                    <FiShoppingCart size={22} color="green" />
                 </Link>
             </Badge>
             <HiOutlineUser size={22} color="green" className="ml-10" />
--- app/components/navbar/Offer.tsx
+++ app/components/navbar/Offer.tsx
@@ -8,9 +8,9 @@ type offer={
 const Offer = ({handleOffer}:offer) => {
     
     return (
-        <div className="flex items-center justify-center bg-black p-1 relative">
+        <div className="flex flex-row items-center justify-center bg-black p-1">
             <p className="text-xs text-gray-200 ">Sign up and <span className="text-white font-semibold">GET 20% OFF</span> for your first order. <span className="underline text-white font-semibold cursor-pointer">Sign up now</span></p>
-            <div className="absolute right-36 cursor-pointer" onClick={handleOffer}>
+            <div className="cursor-pointer pl-2" onClick={handleOffer}>
             <AiOutlineClose color="white" />
             </div>
         </div>
