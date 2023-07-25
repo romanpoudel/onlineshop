8da6f73ea1bbe3f43eb2025aa35e2da2ed318d97
romanpoudel <romanpoudel325@gmail.com>
2023-07-25 18:59:26 +0545

    fixed hydration error

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Navbar from './components/navbar/Navbar'
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import Offer from './components/navbar/Offer'
 import Carousel from './components/Carousel'
 import Products from './components/products/Products'
@@ -12,11 +12,19 @@ export default function Home() {
   const handleShow=()=>{
     setShow(!show);
   }
+  const [hasMounted, setHasMounted] =useState(false);
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+  if (!hasMounted) {
+    return null;
+  }
+  
   return (
     <div className='bg-gray-50'>
-      {
+      {/* {
         show && <Offer handleOffer={handleShow}/>
-      }
+      } */}
       <Navbar />
       <Carousel />
       <Products />
